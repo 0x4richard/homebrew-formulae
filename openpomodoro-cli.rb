cbdb84914d57f009f604abce5f75eef1f1fc1d44
Andrew Patterson <andrew.pattersonap24@googlemail.com>
2022-05-04 11:08:56 +0100

    bin.install instead

--- openpomodoro-cli.rb
+++ openpomodoro-cli.rb
@@ -15,8 +15,8 @@ class OpenpomodoroCli < Formula
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
    #  system "./configure", *std_configure_args, "--disable-silent-rules"
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-    system "go", "install", "github.com/open-pomodoro/openpomodoro-cli/cmd/pomodoro@0.3.0"
-    # bin.install "openpomodoroCli"
+    # system "go", "install", "github.com/open-pomodoro/openpomodoro-cli/cmd/pomodoro@latest"
+    bin.install "openpomodoroCli"
   end
 
   test do
