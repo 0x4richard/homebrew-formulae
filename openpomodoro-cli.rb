e9f83dfb1322651115c5bb6e7e5a1c9e75e7038e
Andrew Patterson <andrew.pattersonap24@googlemail.com>
2022-05-04 10:53:24 +0100

    syntaxfix

--- openpomodoro-cli.rb
+++ openpomodoro-cli.rb
@@ -13,9 +13,9 @@ class OpenpomodoroCli < Formula
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", *std_configure_args, "--disable-silent-rules"
+   #  system "./configure", *std_configure_args, "--disable-silent-rules"
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-    system "go" "get" "-u" "github.com/open-pomodoro/openpomodoro-cli/cmd/pomodoro"
+    system "go", "get", "-u", "github.com/open-pomodoro/openpomodoro-cli/cmd/pomodoro"
     bin.install "openpomodoroCli"
   end
 
